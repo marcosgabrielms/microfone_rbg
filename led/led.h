9f71eaf7bc7b1e809358b1bec71907c66b282b9d
Marcos <marcosgabriel166@gmail.com>
2025-07-23 13:33:08 -0300

    :recycle: refactor: altera estrutura de comentários do arquivo header

--- led/led.h
+++ led/led.h
@@ -4,10 +4,7 @@
 
 #include <stdint.h> // Para o tipo uint8_t
 
-// Inicializa o hardware do LED RGB
-void led_init();
-
-// Define a cor do LED RGB (valores r, g, b: 0-255)
-void led_set_rgb(uint8_t r, uint8_t g, uint8_t b);
+void led_init(); // Inicializa o hardware do LED RGB
+void led_set_rgb(uint8_t r, uint8_t g, uint8_t b); // Define a cor do LED RGB (valores r, g, b: 0-255)
 
 #endif // LED_H
