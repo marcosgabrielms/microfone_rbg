b4f6412e6051a07e25421197019392aad66d3917
Marcos <marcosgabriel166@gmail.com>
2025-07-23 13:35:37 -0300

    :recycle: refactor: remove os limites para o controle de som do arquivo header do mic

--- mic/mic.h
+++ mic/mic.h
@@ -5,9 +5,6 @@
 //Novos limites baseados na amplitude (0 a 4095).
 //Esses valores são apenas para testes, necessitando ajustá-los.
 
-#define LIMITE_SOM_BAIXO 600.0f
-#define LIMITE_SOM_ALTO 1700.0f
-
 void mic_init();
 float mic_get_level();
 
