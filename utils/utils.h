2e6fa9a869eafd108f869931f8b3796b6abcffe7
Marcos <marcosgabriel166@gmail.com>
2025-07-23 13:36:01 -0300

    :recycle: refactor: adiciona valores de controle do mic

--- utils/utils.h
+++ utils/utils.h
@@ -2,4 +2,8 @@
 #ifndef UTILS_H 
 #define UTILS_H // Define UTILS_H para evitar inclusões múltiplas deste header
 
+//Limites de som (Ajuste conforme o ambiente)
+#define LIMITE_SOM_BAIXO 300.0f
+#define LIMITE_SOM_ALTO 700.0f
+
 #endif // Fim da diretiva de inclusão condicional UTILS_H
